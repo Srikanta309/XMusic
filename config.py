a996a8cafce05153ce7d9d9e7fe82ef1d30bb036
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2023-12-30 15:34:29 +0530

    Update config.py
    Signed-off-by: Srikanta309 <139033378+Srikanta309@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/srikanta309/XMusic",
+    "https://te.legra.ph/file/e6471d19bd04a5095436a.mp4",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
