93b857364a63556ea0fd52210b2caba69f454152
Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>
2023-12-31 00:32:16 +0530

    Update config.py
    Signed-off-by: Pi͢͢͢kสcђu <146358318+IR-O@users.noreply.github.com>

--- config.py
+++ config.py
@@ -34,6 +34,10 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/srikanta309/XMusic",
 )
+ALON_BSDK = getenv(
+    "ALON_BSDK",
+    "https://te.legra.ph/file/e6471d19bd04a5095436a.mp4",
+)
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
