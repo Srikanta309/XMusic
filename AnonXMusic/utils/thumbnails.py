9edf7ab92856459641a8c5ec2b77a6c496eae4ea
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2024-01-21 00:00:04 +0530

    Update thumbnails.py
    Signed-off-by: Srikanta309 <139033378+Srikanta309@users.noreply.github.com>

--- AnonXMusic/utils/thumbnails.py
+++ AnonXMusic/utils/thumbnails.py
@@ -7,8 +7,6 @@ import aiofiles
 import aiohttp
 from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
-from unidecode import unidecode
-
 from youtubesearchpython.__future__ import VideosSearch
 from AnonXMusic import app
 from config import YOUTUBE_IMG_URL
