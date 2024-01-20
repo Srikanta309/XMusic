68974e96e9e795fd601080005d8c4bd1a1923d50
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2024-01-20 14:26:01 +0530

    Update config.py
    Signed-off-by: Srikanta309 <139033378+Srikanta309@users.noreply.github.com>

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 ALON_BSDK = getenv(
     "ALON_BSDK",
-    "https://te.legra.ph/file/e6471d19bd04a5095436a.mp4",
+    "https://telegra.ph/file/37b65a7e12aa205051a1b.mp4",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
