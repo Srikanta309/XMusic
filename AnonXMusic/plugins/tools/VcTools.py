4e49ee63093657c2ba7e82bc14378f6461ce5add
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2024-02-13 01:08:12 +0530

    Update VcTools.py
    Signed-off-by: Srikanta309 <139033378+Srikanta309@users.noreply.github.com>

--- AnonXMusic/plugins/tools/VcTools.py
+++ AnonXMusic/plugins/tools/VcTools.py
@@ -21,7 +21,7 @@ async def brah3(app :app, message:Message):
            x = 0
            for user in message.video_chat_members_invited.users:
              try:
-               text += f"{message.from_user.mention} 
+               text += f"f"[{user.first_name}]({from_user.mention}) 
                x += 1
              except Exception:
                pass
