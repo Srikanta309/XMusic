040c72cf62a75b921f654c1f1794f945bf35ace0
Srikanta309 <139033378+Srikanta309@users.noreply.github.com>
2024-02-23 18:25:31 +0530

    Update VcTools.py
    Signed-off-by: Srikanta309 <139033378+Srikanta309@users.noreply.github.com>

--- AnonXMusic/plugins/tools/VcTools.py
+++ AnonXMusic/plugins/tools/VcTools.py
@@ -28,7 +28,7 @@ async def brah3(app :app, message:Message):
            try:
              await message.reply(f"{text} 💞")
            except:
-             pass ({from_user.mention})
+             pass 
 
 
 @app.on_message(filters.command("math", prefixes="/"))
